a8b0518bb6520253d830902993e53f3d4db4f03b
arnoN7 <arnaud.rover@gmail.com>
2019-08-18 17:07:32 +0300

    Update elevator.js
    Pass Challenges 1 to 9 with a more readable code better using API

--- elevator.js
+++ elevator.js
@@ -1,7 +1,12 @@
 {
     init: function(elevators, floors) {
-        var callList = [];
         var loop = 0;
+        for(var i = 0; i < floors.length; i++) {
+            floors[i].up = false;
+            floors[i].down = false;
+            floors[i].elevatorUP = -1;
+            floors[i].elevatorDOWN = -1;
+        }
 
         function addToRide(floorNum, elevator) {
             if(elevator.goingUpIndicator()==true && elevator.goingDownIndicator()==true) {
@@ -17,132 +22,153 @@
             }
 
         }
-
-        function isItemInArray(array, item) {
-            for (var i = 0; i < array.length; i++) {
-                // This if statement depends on the format of your array
-                if (array[i][0] == item[0] && array[i][1] == item[1] && array[i][2] == item[2]) {
-                    console.log(`[WARNING Double Call] ${item}`);
-                    return false;   // Found it
-                }
-            }
-            return false;   // Not found
-        }
         
         function pileCall(floorNum, direction) {
-            console.log(`[Pile Call] ${floorNum}${direction} size:${callList.length+1}`);
+            console.log(`[Pile Call] ${floorNum}${direction}`);
             loop ++;
             if (loop > 1000) {
                 console.log(`loop`);
             }
-            if(direction == "up" && isItemInArray(callList, [floorNum, 1, 0]) == false){
-                callList.push([floorNum, 1, 0]);
-            } else if(direction == "down" && isItemInArray(callList, [floorNum, 0, 1]) == false) {
-                callList.push([floorNum, 0, 1]);
+            if(direction === "up") {
+                floors[floorNum].up = true;
+            } else if (direction === "down") {
+                floors[floorNum].down = true;
             }
         }
 
         function registerCall(floorNum, direction, elevator) {
-            if(elevator.goingUpIndicator()==true && elevator.goingDownIndicator()==true) {
-                if (direction == "up") {
-                    console.log(`[Will Go UP Called] ${floorNum}`);
-                    elevator.goingDownIndicator(false);
-                    elevator.goingUpIndicator(true);
-                } else if (direction == "down"){
-                    elevator.goingDownIndicator(true);
-                    elevator.goingUpIndicator(false);
-                    console.log(`[Will Go Down Called] ${floorNum}`);
-                }
-                addToRide(floorNum, elevator);
-            } else if (elevator.goingUpIndicator()==true && direction =="up" && 
-                       Math.min(elevator.currentFloor(), elevator.destinationQueue[0]) < floorNum) {
-                console.log(`[Opport Catch UP] ${floorNum}`);
-                addToRide(floorNum, elevator);     
-            } else if (elevator.goingDownIndicator()==true && direction =="down" && 
-                       Math.max(elevator.currentFloor(), elevator.destinationQueue[0]) > floorNum) {
-                console.log(`[Opport Catch Down] ${floorNum}`);
-                addToRide(floorNum, elevator); 
-            } else {
-                return false;
+            switch (elevator.status) {
+                case 'idle':
+                    if (direction == "up") {
+                        console.log(`[Catch Go UP Called] ${floorNum}`);
+                        elevator.goingDownIndicator(false);
+                        elevator.goingUpIndicator(true);
+                        floors[floorNum].elevatorUP = elevator.id;
+                    } else if (direction == "down"){
+                        elevator.goingDownIndicator(true);
+                        elevator.goingUpIndicator(false);
+                        floors[floorNum].elevatorDOWN = elevator.id;
+                        console.log(`[Catch Go Down Called] ${floorNum}`);
+                    }
+                    elevator.status = "catch";
+                    addToRide(floorNum, elevator);
+                    return true;
+                case 'deliver':
+                    if (direction == "up") {
+                        console.log(`[Opport Catch UP Called] ${floorNum} ${elevator.id}`);
+                        floors[floorNum].elevatorUP = elevator.id;
+                    } else if (direction == "down"){
+                        console.log(`[Opport Catch DOWN Called] ${floorNum} ${elevator.id}`);
+                        floors[floorNum].elevatorDOWN = elevator.id;
+                    }
+                    addToRide(floorNum, elevator);
+                    return false;
+                case 'catch':
+                    console.log(`[ERROR] NO REGISTER CALL IN CATCH`);
+                    return false;
             }
-            return true;
+            return false;
 
         }
 
-        function unpileCall(elevator) {
+        function unpile(elevator) {
             console.log(`[UnPile]`);
-            var nbCall = 0;
-            var callListCopy;
-            //If was going up try to find someone going down
-            if(elevator.goingUpIndicator()) {
-                callList.sort(function(a, b) {
-                    return b[0] - a[0];
-                });
-                console.log(`Trying to go down`);
-                callListCopy = callList.slice();
-                callListCopy.forEach(function(call, index, object) {
-                    if (call[2] === 1){
-                        if(nbCall == 0) {
-                            elevator.goingDownIndicator(true);
-                            elevator.goingUpIndicator(true);
+            var distance = floors.length + 1;
+            var call = new Object();
+            for (var i= 0; i < floors.length; i++) {
+                if((floors[i].elevatorDOWN == -1) && (floors[i].down == true) 
+                   || (floors[i].elevatorUP == -1) && (floors[i].up == true)) {
+                    if( Math.abs(elevator.currentFloor() - i) < distance) {
+                        distance = Math.abs(elevator.currentFloor() - i);
+                        call.floor = i;
+                        if(floors[i].up) {
+                            call.direction = "up";
+                        } else {
+                            call.direction = "down";
                         }
-                        var floorCalling = call[0];
-                        callList.splice(index-nbCall,1);
-                        registerCall(floorCalling, "down", elevator);
-                        nbCall++;
                     }
-                });
-            }
-            //If was going down try to find someone going up
-            if(elevator.goingDownIndicator() && nbCall == 0) {
-                callList.sort(function(a, b) {
-                    return a[0] - b[0];
-                });
-                console.log(`Trying to go up`);
-                callListCopy = callList.slice();
-                callListCopy.forEach(function(call, index, object) {
-                    if (call[1] === 1){
-                        if(nbCall == 0) {
-                            elevator.goingDownIndicator(true);
-                            elevator.goingUpIndicator(true);
-                        }
-                        var floorCalling = call[0];
-                        callList.splice(index-nbCall,1);
-                        registerCall(floorCalling, "up", elevator);
-                        nbCall++;
-                    }
-                });
-            }
-            if(nbCall == 0) {
-                elevator.goingDownIndicator(true);
-                elevator.goingUpIndicator(true);
-                if(callList.length > 0) {
-                    console.log(`[No Optimised call take the first] ${callList}`);
-                    var floorCalling = callList[0][0]
-                    if(callList[0][1] ===1) {
-                        callList.splice(0,1);
-                        registerCall(floorCalling, "up", elevator);
-                    } else {
-                        callList.splice(0,1);
-                        registerCall(floorCalling, "down", elevator);
-                    }
+                    
                 }
-
+            }
+            if(distance < floors.length + 1) {
+                registerCall(call.floor, call.direction, elevator);
             }
         }
+        function unpileCall(floorNum, direction, elevator) {
+            switch (elevator.status) {
+                case 'deliver' :
+                case 'catch':
+                    if(elevator.goingDownIndicator() && elevator.goingUpIndicator()) {
+                        console.log(`[Error UnPileCall] Direction is not set`);
+                    } else if (elevator.goingDownIndicator()) {
+                        floors[floorNum].down = false;
+                        floors[floorNum].elevatorDOWN = -1;
+                    } else if (elevator.goingUpIndicator()) {
+                        floors[floorNum].up = false;
+                        floors[floorNum].elevatorUP = -1;
+                    }
+                    break;
+                case 'idle' :
+                    break;
+            }
+        }
+        
         // Whenever the elevator is idle (has no more queued destinations) ...
         elevators.forEach((elevator, index) => {
             elevator.id = index;
+            elevator.status = "idle";
+            elevator.personWeigth = 1 / elevator.maxPassengerCount();
             elevator.on("idle", function() {
                 // let's go to all the floors (or did we forget one?)
-                unpileCall(elevator);
+                if(elevator.status == "deliver") {
+                    elevator.goingDownIndicator(true);
+                    elevator.goingUpIndicator(true);
+                    elevator.status = "idle";
+                }
+                unpile(elevator);
             });
             elevator.on("floor_button_pressed", function(floorNum) {
                 if(addToRide(floorNum, elevator) == false) {
                     console.log(`[ERROR NO GO Destination] ${floorNum}${elevator}`);
                 } else {
                     console.log(`[Go To]${floorNum}`);
+                    elevator.status = "deliver";
                 }
+            });
+            elevator.on("passing_floor", (floorNum, direction) => {
+                //console.log(`Passing Floor ${floorNum} ${direction} ${elevator.id}`);
+                switch (elevator.status) {
+                    case 'deliver' :
+                        //Opportunity Catch
+                        if(elevator.goingDownIndicator() && elevator.goingUpIndicator()) {
+                            console.log(`[Error Passing Floor] Direction is not set`);
+                        } else if (elevator.loadFactor() < (1-elevator.personWeigth)) {
+                            //Space available in elevator
+                            if (elevator.goingDownIndicator() && (floors[floorNum].down == true)) {
+                                //Going Down
+                                if(floors[floorNum].elevatorDOWN == -1 || floors[floorNum].elevatorDOWN == elevator.id) {
+                                    console.log(`Passing Floor Opportunity Catch Down ${floorNum} ${elevator.id}`);
+                                    addToRide(floorNum, elevator); 
+                                }
+                            } else if (elevator.goingUpIndicator() && (floors[floorNum].up == true)) {
+                                //Going Up
+                                if(floors[floorNum].elevatorUP == -1 || floors[floorNum].elevatorUP == elevator.id) {
+                                    console.log(`Passing Floor Opportunity Catch Up ${floorNum} ${elevator.id}`);
+                                    addToRide(floorNum, elevator); 
+                                }
+                            } else {
+                                console.log(`catch scheduled UP by ${floors[floorNum].elevatorUP}, DOWN ${floors[floorNum].elevatorDOWN}`);
+                            }
+                        } else {
+                            console.log(`[Elevator FULL ?!] ${elevator.loadFactor()}`);
+                        }
+                        break;
+                }
+            });
+            elevator.on("stopped_at_floor", (floorNum, direction) => {
+                //console.log(`Stopped Floor ${floorNum} ${direction} ${elevator.id}`);
+                unpileCall(floorNum, direction, elevator);
+                
             });
         });
 
@@ -152,29 +178,30 @@
                 console.log(`floor${floorNum} [up_button_pressed]`);
                 var isAccepted = false;
                 var i = 0;
-                while (isAccepted || i == elevators.length) {
-                    isAccepted = registerCall(floorNum, "up", elevators[i]);
+                while (!isAccepted && i < elevators.length) {
+                    if(elevators[i].status == 'idle') {
+                        isAccepted = registerCall(floorNum, "up", elevators[i]);
+                    }
                     i++;
                 }
-                if (!isAccepted) {
-                    pileCall(floorNum, "up");
-                }
+                pileCall(floorNum, "up");
             });
             floor.on("down_button_pressed", () => {
                 console.log(`floor${floorNum} [down_button_pressed]`);
                 var isAccepted = false;
                 var i = 0;
-                while (isAccepted || i == elevators.length) {
-                    isAccepted = registerCall(floorNum, "down", elevators[i]);
+                while (!isAccepted && i < elevators.length) {
+                    if(elevators[i].status == 'idle') {
+                        isAccepted = registerCall(floorNum, "down", elevators[i]);  
+                    }
                     i++;
                 }
-                if (!isAccepted) {
-                    pileCall(floorNum, "down");
-                }
+                pileCall(floorNum, "down");
             });
+
         });
     },
-        update: function(dt, elevators, floors) {
-            // We normally don't need to do anything here
-        }
+    update: function(dt, elevators, floors) {
+        // We normally don't need to do anything here
+    }
 }
